1d4d90263c38a223c7d041267686ceaf5574a4e7
Phoenix Isaac Pereira <phoenixpereira99@gmail.com>
2024-12-08 10:38:44 +1030

    Initial Commit

---
+++ weatherify/weatherifyApp.swift
@@ -0,0 +1,17 @@
+//
+//  weatherifyApp.swift
+//  weatherify
+//
+//  Created by Phoenix Pereira on 8/12/2024.
+//
+
+import SwiftUI
+
+@main
+struct weatherifyApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
